4575e628cc5d181f8fa51ef46299cd68c815047b
MoncoJS <moncojs@gmail.com>
2025-06-06 08:29:28 +0700

    Get Del Route

--- backend/server.js
+++ backend/server.js
@@ -38,22 +38,68 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Expense Tracker API");
 });
 
+app.get("/api/transactions/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    console.log(userId);
+
+    const transactions = await sql`
+        SELECT * FROM Transactions
+        WHERE user_id = ${userId} ORDER BY created_at DESC
+        `;
+
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log("Error getting transactions:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.post("/api/transactions", async (req, res) => {
   try {
     const { title, amount, category, user_id } = req.body;
-    if (!title || !amount || !category || !user_id === undefined) {
+    if (!title || !amount || !category || !user_id) {
       return res.status(400).json({ message: "All fields are required" });
     }
-    await sql`
+    const transaction = await sql`
       INSERT INTO Transactions (user_id, title, amount, category)
       VALUES (${user_id}, ${title}, ${amount}, ${category})
       RETURNING *
     `;
 
-    console.log(transaction);
     res.status(201).json(transaction[0]);
   } catch (error) {
-    console.log("Error creating transaction:", error);
+    console.error("Error creating transaction:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ message: "Invalid transaction ID" });
+    }
+
+    const result = await sql`
+        DELETE FROM Transactions
+        WHERE id = ${id}
+        RETURNING *
+        `;
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    res
+      .status(200)
+      .json({
+        message: "Transaction deleted successfully",
+        transaction: result[0],
+      });
+  } catch (error) {
+    console.error("Error deleting transaction:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
