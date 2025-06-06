5b54c35f49b4bc570831866b15ea67a0aba45ec0
MoncoJS <moncojs@gmail.com>
2025-06-06 07:30:14 +0700

    Backend Setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB(process.env.DATABASE_URL);
 
 app.get("/", (req, res) => {
   res.send("It, Works! 123");
 });
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server is running on port:", PORT);
 });
